752636ff9f585584411af10db069cf0b223d3007
Bucket-List <thomas.arnaud2021@campus-eni.fr>
2022-02-08 15:22:43 +0100

    page accueil avec la liste des sorties et tous les filtres

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,13 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Sortie;
-use App\Repository\ParticipantRepository;
 use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,15 +13,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'accueil')]
-    public function index(SiteRepository        $siteRepository,
-                          SortieRepository      $sortieRepository,
-                          ParticipantRepository $participantRepository,
+    public function index(SiteRepository   $siteRepository,
+                          SortieRepository $sortieRepository,
 
     ): Response
 
     {
         $sites = $siteRepository->findAll();
         $sorties = $sortieRepository->findAll();
+        return $this->render('accueil/index.html.twig', [
+            "sorties" => $sorties,
+            "sites" => $sites,
+        ]);
+    }
+
+    #[Route('/accueil/recherche', name: 'recherche')]
+    public function rechercheParFiltre(SiteRepository   $siteRepository,
+                                       SortieRepository $sortieRepository,
+                                       Request          $request,
+    ): Response
+
+    {
+        $choixSite = $request->request->get('sites');
+        $choixSearch = $request->request->get('search');
+        $choixDateStart = $request->request->get('trip-start');
+        $choixDateEnd = $request->request->get('trip-end');
+        $choixOrganisateur = $request->request->get('organisateur');
+        $choixInscrit = $request->request->get('inscrit');
+        $choixPasInscrit = $request->request->get('pasInscrit');
+        $choixPassee = $request->request->get('passee');
+        $sorties = $sortieRepository->findAll();
+        $sites = $siteRepository->findAll();
         return $this->render('accueil/index.html.twig', [
             "sorties" => $sorties,
             "sites" => $sites,
