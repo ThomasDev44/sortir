44abc691dd0e78af24bbe9500c397fe25877f465
MaRiOn202 <77493223+MaRiOn202@users.noreply.github.com>
2022-02-17 09:37:27 +0100

    css redirection twig

--- src/Controller/RedirectionController.php
+++ src/Controller/RedirectionController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class RedirectionController extends AbstractController
 {
     #[Route('/redirection', name: 'redirection')]
     public function index(): Response
     {
+
+       /* if ($this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }*/
+
+    #
+
         return $this->render('reset_password/redirection.html.twig', [
             'controller_name' => 'RedirectionController',
         ]);
